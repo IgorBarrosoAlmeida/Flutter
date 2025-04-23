74bac2e4a9c3a72fd770e9f6493655b064dc4058
Igor Barroso Almeida <igorba867@gmail.com>
2025-04-23 20:11:49 -0300

    call API

--- Flutter/flutter_basico/lib/pages/page_one.dart
+++ Flutter/flutter_basico/lib/pages/page_one.dart
@@ -1,7 +1,8 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_basico/widgets/custom_button_widget.dart';
+import 'package:http/http.dart' as http;
 
 class PageOne extends StatefulWidget {
   const PageOne({super.key});
@@ -11,12 +12,18 @@ class PageOne extends StatefulWidget {
 }
 
 class _PageOneState extends State<PageOne> {
-  ValueNotifier<int> randomValue = ValueNotifier<int>(0);
+  ValueNotifier<List<Post>> posts = ValueNotifier<List<Post>>([]);
 
-  void changeRandomValue() async {
-    for (int i = 0; i < 10; i++) {
-      await Future.delayed(Duration(seconds: 1));
-      randomValue.value = Random().nextInt(1000);
+  callAPI() async {
+    var client = http.Client();
+    try {
+      var response = await client.get(
+        Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+      );
+      var decodedResponse = jsonDecode(response.body) as List;
+      posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
+    } finally {
+      client.close();
     }
   }
 
@@ -24,23 +31,45 @@ class _PageOneState extends State<PageOne> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ValueListenableBuilder(
-              valueListenable: randomValue,
-              builder: (context, value, __) {
-                return Text("O valor eh: $value");
-              },
-            ),
-            CustomButtonWidget(
-              onPressed: changeRandomValue,
-              title: "Button",
-              isDisable: false,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ValueListenableBuilder<List<Post>>(
+                valueListenable: posts,
+                builder: (context, value, __) {
+                  return ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: value.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(title: Text(value[index].title));
+                    },
+                  );
+                },
+              ),
+              CustomButtonWidget(
+                onPressed: callAPI,
+                title: "Button",
+                isDisable: false,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body);
+
+  factory Post.fromJson(Map json) {
+    return Post(json['userId'], json['id'], json['title'], json['body']);
   }
 }
